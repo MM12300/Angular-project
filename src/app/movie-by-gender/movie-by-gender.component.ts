113c872c98c9a95da82116d2ac21d3135422b641
MM12300 <nerggalkore@gmail.com>
2022-03-04 18:11:52 +0100

    trick to update movie-detail component

--- src/app/movie-by-gender/movie-by-gender.component.ts
+++ src/app/movie-by-gender/movie-by-gender.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Movie} from "../../interfaces/movies";
 import {ApiService} from "../../services/api.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-movie-by-gender',
@@ -12,14 +12,20 @@ export class MovieByGenderComponent implements OnInit {
 
   public moviesByGenre!: Movie[]
 
-  constructor(private apiService: ApiService, private route: ActivatedRoute) { }
+  constructor(private apiService: ApiService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    //location.reload();
+  this.getMovies();
+  this.router.events.subscribe((val) => {
+    this.getMovies();
+  });
+  }
+
+
+  getMovies(){
+    const genre_id = Number(this.route.snapshot.paramMap.get('id'));
     this.apiService.getAllMovies().subscribe(movies => {
-      const genre_id = Number(this.route.snapshot.paramMap.get('id'));
       this.moviesByGenre = movies.filter(movie => movie.genre_ids.includes(genre_id));
     });
-
   }
 }
