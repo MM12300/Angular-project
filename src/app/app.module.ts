a1e0a4dc3985dbc64ff9da5c4fd8bcd16517543c
MM12300 <nerggalkore@gmail.com>
2022-03-07 10:37:41 +0100

    css improvements and movies-list refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MovieByGenderComponent } from './movie-by-gender/movie-by-gender.compon
 
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MoviesListComponent } from './movies-list/movies-list.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MovieDetailComponent,
     MovieAddCommentComponent,
     SidebarComponent,
-    MovieByGenderComponent
+    MovieByGenderComponent,
+    MoviesListComponent
   ],
   imports: [
     BrowserModule,
